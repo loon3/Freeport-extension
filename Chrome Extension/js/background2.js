b3f92a1df517ee0efaa6dd984f6630099e3bb1d3
Joe Looney <hello@joelooney.org>
2019-02-15 14:31:57 -0500

    Update background2.js

--- Chrome Extension/js/background2.js
+++ Chrome Extension/js/background2.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.storage.local.get(['instances'], function(result) {  
-		if (!result.instances || result.instances == 0){
-			chrome.storage.local.set({'instances': 1}, function() {	
+// 	chrome.storage.local.get(['instances'], function(result) {  
+// 		if (!result.instances || result.instances == 0){
+// 			chrome.storage.local.set({'instances': 1}, function() {	
         			chrome.tabs.create({'url': chrome.extension.getURL('../html/tab.html')}, function(tab) {});
-			})
-		} else {
-			window.alert("Freeport is already open!")
-		}
-	})
+// 			})
+// 		} else {
+// 			window.alert("Freeport is already open!")
+// 		}
+// 	})
 })
